2e4a05ee821c14681fbe9d02c3fb247b59616550
Adam <296786475@qq.com>
2021-12-20 18:36:38 +0800

    Initial Commit

---
+++ Adam_20211220_SwiftUIStudy/Adam_20211220_SwiftUIStudyApp.swift
@@ -0,0 +1,20 @@
+//
+//  Adam_20211220_SwiftUIStudyApp.swift
+//  Adam_20211220_SwiftUIStudy
+//
+//  Created by Adam on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct Adam_20211220_SwiftUIStudyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
