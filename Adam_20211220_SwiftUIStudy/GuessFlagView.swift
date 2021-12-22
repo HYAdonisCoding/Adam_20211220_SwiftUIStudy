d40a77703de6dead85fe368b60d217791aa63740
Adam <>
2021-12-22 10:55:03 +0800

    Add GuessFlagView

---
+++ Adam_20211220_SwiftUIStudy/GuessFlagView.swift
@@ -0,0 +1,20 @@
+//
+//  GuessFlagView.swift
+//  Adam_20211220_SwiftUIStudy
+//
+//  Created by Adam on 2021/12/22.
+//
+
+import SwiftUI
+
+struct GuessFlagView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GuessFlagView_Previews: PreviewProvider {
+    static var previews: some View {
+        GuessFlagView()
+    }
+}
